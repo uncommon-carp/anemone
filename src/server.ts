7cc2c4f333c3a8fb5e4a1439f8b7c90c383ccf9c
Cory Sorel <Cory@Lorenas-MacBook-Air.local>
2026-06-22 16:01:29 -0600

    type fixes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, NextFunction } from 'express';
+import express, { type Request, type Response, type NextFunction } from 'express';
 
 const app = express();
 app.use(express.json());
